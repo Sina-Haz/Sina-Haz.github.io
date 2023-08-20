0efc400453b0a45b83ef9ec2e2bf4e0ce0026c6a
Sina-Haz <101819919+Sina-Haz@users.noreply.github.com>
2023-08-20 11:44:16 -0400

    trying to fix dependencies again

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,6 +9,21 @@ import { fadeIn, textVariant } from "../utils/motion";
 const About = () => {
   return (
     <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p variants = {fadeIn('','',0.2, 1)} className="leading-loose mb-4">
+        <div className="mb-2">
+          I'm a computer science and math major at Rutgers University. I'm a fullstack developer with
+          experience with Android Studio, Reactjs, Threejs, JavaFX, and Vite.
+        </div>
+
+        <div className="mb-2">
+          I also write low-level applications with C/C++, and work on AI/ML projects with Python, Jupyter,
+          PyTorch, and <a href='' className='underline-offset-auto underline'>VectorGrad</a>
+        </div>
+      </motion.p>
     </>
   );
 }
